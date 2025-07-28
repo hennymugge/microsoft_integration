c6ae4d60511dd82a6b19aa99f96590803e25493e
Hennymugge <hennymugge@gmail.com>
2025-07-28 12:38:17 +0000

    add setting for default role

---
+++ microsoft_integration/microsoft_integration/doctype/microsoft_integration_settings/microsoft_integration_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Castlecraft Ecommerce Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Microsoft Integration Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
