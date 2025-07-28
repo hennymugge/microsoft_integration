8f0422d3e0af0cc9f13d57ba0dd9c3c931ea51d8
Hennymugge <hennymugge@gmail.com>
2025-07-28 12:30:41 +0000

    feat: Add standard DocTypes for SSO mappings and settings

---
+++ microsoft_integration/microsoft_integration/doctype/frappe_role_entra_role/frappe_role_entra_role.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Castlecraft Ecommerce Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Frappe Role Entra Role", {
+// 	refresh(frm) {
+
+// 	},
+// });
